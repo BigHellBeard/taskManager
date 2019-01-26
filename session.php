e68d5f7822f15eb9934ba3ffc1ad07a106dbff7e
Aleksey Gulko <aleshaakadick@gmail.com>
2019-01-26 18:14:33 +0300

    cookie fix

--- session.php
+++ session.php
@@ -15,13 +15,13 @@ if (!$_SESSION['authCheck'] && !empty($uri)) {
 	exit();
 }
 if ($_POST['logout']) {
-	setcookie(session_name(), session_id(), time() - 300);
+	setcookie(session_name(), session_id(), time() - 300, "/");
 	session_destroy();
 	unset($_SESSION['authCheck']);
-	header('Location: http://' . $_SERVER['HTTP_HOST'] . '/');
+	header('Location: /');
 	exit();
 }
 if ($_SESSION['authCheck']) {
-	setcookie(session_name(), session_id(), time() + $lifetime);
-	setcookie('user_cookie', $_COOKIE['user_cookie'], time() + (60 * 60 * 24 * 30));
+	setcookie(session_name(), session_id(), time() + $lifetime, "/");
+	setcookie('user_cookie', $_COOKIE['user_cookie'], time() + (60 * 60 * 24 * 30), "/");
 }
